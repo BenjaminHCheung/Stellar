ca1aa117eee95ec2cc58ffcbf4e23bf3636fa8d9
Benjamin Cheung <benjaminhcheung@gmail.com>
2018-11-08 12:23:41 -0700

    created probability library

---
+++ probabilitymath.cpp
@@ -0,0 +1,6 @@
+#include "probabilitymath.h"
+
+double calculate_hit_probability(Starship* attacker, Starship* defender)
+{
+
+}
---
+++ probabilitymath.h
@@ -0,0 +1,8 @@
+#ifndef PROBABILITYMATH_H
+#define PROBABILITYMATH_H
+
+#include "starship.h"
+
+double calculate_hit_probability(Starship* attacker, Starship* defender);
+
+#endif // PROBABILITYMATH_H
