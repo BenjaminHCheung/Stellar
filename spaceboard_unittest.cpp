037faa9744d152a71239930710c3f4f967bf4ee5
Benjamin Cheung <benjaminhcheung@gmail.com>
2018-12-13 18:28:31 -0700

    Written First Unit Test

--- spaceboard_unittest.cpp
+++ spaceboard_unittest.cpp
@@ -1,42 +1,66 @@
 #include "gtest/gtest.h"
 #include "spaceboard.h"
 
-TEST(GivenAShipOfAppropriateSizeTeamNumberAndLocation, WhenUsingAddShipFunction_AddsShipToCorrectTeamVectorList)
+class SpaceBoardTest: public ::testing::Test
 {
+protected:
+    SpaceBoard* mSpaceBoard;
 
+    virtual void SetUp()
+    {
+        mSpaceBoard = new SpaceBoard();
+    }
+    virtual void TearDown()
+    {
+        delete mSpaceBoard;
+    }
+};
+
+TEST(GivenAShipOfAppropriateSizeAndLocationGoingIntoTeamOneList, WhenUsingAddShipFunction_AddsShipToTheCorrectTeamVectorListWithCorrectValues)
+{
+    SpaceBoard mySpaceBoard{SpaceBoard()};
+    mySpaceBoard.add_starship(1,3,0,0,0);
+    unsigned long long int expectedSize{1};
+    unsigned long long int returnedSize{mySpaceBoard.get_team_one_ships()->size()};
+    EXPECT_EQ(expectedSize, returnedSize);
 }
 
-TEST(GivenAPlanetOfApproriateSizeAndLocation, WhenUsingTheAddStellarBodyFunction_AddsPlanetToCorrectListWithCorrectValues)
-{
+//TEST(GivenAShipOfAppropriateSizeAndLocationGoingIntoTeamTwoList, WhenUsingAddShipFunction_AddsShipToTheCorrectTeamVectorListWithCorrectValues)
+//{
 
-}
+//}
 
-TEST(GivenACommandToGenerateAllShips, WhenCallingTheGenerateShipsListsFunction_BothTeamListsAreFilledWithTheDefaultListOfShipsPositionedInALine)
-{
+//TEST(GivenAPlanetOfApproriateSizeAndLocation, WhenUsingTheAddStellarBodyFunction_AddsPlanetToCorrectListWithCorrectValues)
+//{
 
-}
+//}
 
-TEST(GivenACommandToGenerateAllStellarBodies, WhenCallingTheGeneratePlanetLitsFunction_TheStellarBodyListFillsWithAPlanetInTheCorrectLocationAndASecondPlanetPositionedOnTheOppositeSideOfTheOrigin)
-{
+//TEST(GivenACommandToGenerateAllShips, WhenCallingTheGenerateShipsListsFunction_BothTeamListsAreFilledWithTheDefaultListOfShipsPositionedInALine)
+//{
 
-}
+//}
 
-TEST(GivenACommandToGenerateDefaultLists, WhenUsingTheGenerateDefaultListsFunction_AllThreeListsAreFilledCorrectly)
-{
+//TEST(GivenACommandToGenerateAllStellarBodies, WhenCallingTheGeneratePlanetLitsFunction_TheStellarBodyListFillsWithAPlanetInTheCorrectLocationAndASecondPlanetPositionedOnTheOppositeSideOfTheOrigin)
+//{
 
-}
+//}
 
-TEST(GivenACommandToClearTheShipLists, WhenUsingTheClearShipsFunction_AllShipsAreClearedFromBothLists)
-{
+//TEST(GivenACommandToGenerateDefaultLists, WhenUsingTheGenerateDefaultListsFunction_AllThreeListsAreFilledCorrectly)
+//{
 
-}
+//}
 
-TEST(GivenACommandToClearTheStellarBodyList, WhenUsingTheClearPlanetsFunction_AllPlanetEntriesAreRemovedFromTheList)
-{
+//TEST(GivenACommandToClearTheShipLists, WhenUsingTheClearShipsFunction_AllShipsAreClearedFromBothLists)
+//{
 
-}
+//}
 
-TEST(GivenACommandToClearLists, WhenUsingTheClearListsFunction_AllListsAreClearedOfAllEntries)
-{
+//TEST(GivenACommandToClearTheStellarBodyList, WhenUsingTheClearPlanetsFunction_AllPlanetEntriesAreRemovedFromTheList)
+//{
 
-}
+//}
+
+//TEST(GivenACommandToClearLists, WhenUsingTheClearListsFunction_AllListsAreClearedOfAllEntries)
+//{
+
+//}
