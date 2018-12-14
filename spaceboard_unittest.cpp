e75d75aaa4fe6a207bfb167dc2cc5d0c6c59b992
Benjamin Cheung <benjaminhcheung@gmail.com>
2018-12-13 22:20:58 -0700

    updated all unit test for spaceboard to be current

--- spaceboard_unittest.cpp
+++ spaceboard_unittest.cpp
@@ -16,39 +16,62 @@ protected:
     }
 };
 
-TEST(GivenAShipOfAppropriateSizeAndLocationGoingIntoTeamOneList, WhenUsingAddShipFunction_AddsShipToTheCorrectTeamVectorListWithCorrectValues)
+TEST(GivenAShipOfAppropriateSizeAndLocationGoingIntoTeamOneList, WhenUsingAddShipFunction_AddsShipToTheCorrectTeamVectorList)
 {
     SpaceBoard mySpaceBoard{SpaceBoard()};
+    unsigned long long int emptyList{0};
+    EXPECT_EQ(emptyList, mySpaceBoard.get_team_one_ships()->size());
     mySpaceBoard.add_starship(1,3,0,0,0);
     unsigned long long int expectedSize{1};
-    unsigned long long int returnedSize{mySpaceBoard.get_team_one_ships()->size()};
-    EXPECT_EQ(expectedSize, returnedSize);
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_team_one_ships()->size());
 }
 
-//TEST(GivenAShipOfAppropriateSizeAndLocationGoingIntoTeamTwoList, WhenUsingAddShipFunction_AddsShipToTheCorrectTeamVectorListWithCorrectValues)
-//{
+TEST(GivenAShipOfAppropriateSizeAndLocationGoingIntoTeamTwoList, WhenUsingAddShipFunction_AddsShipToTheCorrectTeamVectorList)
+{
+    SpaceBoard mySpaceBoard{SpaceBoard()};
+    unsigned long long int emptyList{0};
+    EXPECT_EQ(emptyList, mySpaceBoard.get_team_two_ships()->size());
+    mySpaceBoard.add_starship(2,4,5,11,8);
+    unsigned long long int expectedSize{1};
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_team_two_ships()->size());
+}
 
-//}
+TEST(GivenAPlanetOfApproriateSizeAndLocation, WhenUsingTheAddStellarBodyFunction_AddsPlanetToCorrectList)
+{
+    SpaceBoard mySpaceBoard{SpaceBoard()};
+    unsigned long long int emptyList{0};
+    EXPECT_EQ(emptyList, mySpaceBoard.get_stellar_body_lists()->size());
+    mySpaceBoard.add_stellar_body(3,0,0,0);
+    unsigned long long int expectedSize{1};
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_stellar_body_lists()->size());
+}
 
-//TEST(GivenAPlanetOfApproriateSizeAndLocation, WhenUsingTheAddStellarBodyFunction_AddsPlanetToCorrectListWithCorrectValues)
-//{
+TEST(GivenACommandToGenerateAllShips, WhenCallingTheGenerateShipsListsFunction_BothTeamListsAreFilledWithTheDefaultListOfShipsPositionedInALine)
+{
+    SpaceBoard mySpaceBoard{SpaceBoard()};
+    mySpaceBoard.generate_ships_lists();
+    unsigned long long int expectedSize{12};
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_team_one_ships()->size());
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_team_two_ships()->size());
+}
 
-//}
+TEST(GivenACommandToGenerateAllStellarBodies, WhenCallingTheGeneratePlanetLitsFunction_TheStellarBodyListFillsWithAPlanetInTheCorrectLocationAndASecondPlanetPositionedOnTheOppositeSideOfTheOrigin)
+{
+    SpaceBoard mySpaceBoard{SpaceBoard()};
+    mySpaceBoard.generate_planet_list();
+    unsigned long long int expectedSize{12};
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_stellar_body_lists()->size());
+}
 
-//TEST(GivenACommandToGenerateAllShips, WhenCallingTheGenerateShipsListsFunction_BothTeamListsAreFilledWithTheDefaultListOfShipsPositionedInALine)
-//{
-
-//}
-
-//TEST(GivenACommandToGenerateAllStellarBodies, WhenCallingTheGeneratePlanetLitsFunction_TheStellarBodyListFillsWithAPlanetInTheCorrectLocationAndASecondPlanetPositionedOnTheOppositeSideOfTheOrigin)
-//{
-
-//}
-
-//TEST(GivenACommandToGenerateDefaultLists, WhenUsingTheGenerateDefaultListsFunction_AllThreeListsAreFilledCorrectly)
-//{
-
-//}
+TEST(GivenACommandToGenerateDefaultLists, WhenUsingTheGenerateDefaultListsFunction_AllThreeListsAreFilledCorrectly)
+{
+    SpaceBoard mySpaceBoard{SpaceBoard()};
+    mySpaceBoard.generate_default_lists();
+    unsigned long long int expectedSize{12};
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_team_one_ships()->size());
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_team_two_ships()->size());
+    EXPECT_EQ(expectedSize, mySpaceBoard.get_stellar_body_lists()->size());
+}
 
 //TEST(GivenACommandToClearTheShipLists, WhenUsingTheClearShipsFunction_AllShipsAreClearedFromBothLists)
 //{
