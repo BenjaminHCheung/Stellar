4b722f3b562e21b379eb80f9ec5f44614579ec5e
Benjamin Cheung <benjaminhcheung@gmail.com>
2018-12-14 22:14:36 -0700

    wrote distance equation but it does not pass the test, debugging that

--- probabilitymath.cpp
+++ probabilitymath.cpp
@@ -1,6 +1,15 @@
 #include "probabilitymath.h"
+#include <math.h>
 
 double calculate_distance(Starship* attacker, Starship* defender)
 {
-    return 0;
+    double differenceInX{attacker->get_position_node()->get_position().get_x_value()
+                        - defender->get_position_node()->get_position().get_x_value()};
+    double differenceInY{attacker->get_position_node()->get_position().get_y_value()
+                        - defender->get_position_node()->get_position().get_y_value()};
+    double differenceInZ{attacker->get_position_node()->get_position().get_z_value()
+                        - defender->get_position_node()->get_position().get_z_value()};
+
+    double distance{sqrt(pow(differenceInX, 2) + pow(differenceInY, 2) + pow(differenceInZ, 2))};
+    return distance;
 }
